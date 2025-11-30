f44e20483d82eb668cc3f0f3c5dceaee2ed86a82
M-GreySun <mgreysun@gmail.com>
2025-11-29 21:41:19 -0500

    Clean up all the files by removed unused and code that was commented out. The css styles were also removed from the js file for readability. It was still good practice changing the DOM with js. Last but not the grid with updated with flex-box attributes. The grid now has a max size and the elements in the grid will take up all the available space. As opposed to having a fixed size for the elements.

--- logic.js
+++ logic.js
@@ -1,70 +1,44 @@
 
 let row=4;
-let column=4;
-let container= document.createElement("div");
-container.setAttribute("style", "display: flex; flex-direction: column; align-items: center; width: 960px; heigth: 960px;");
+let col=4;
 
+let container= document.createElement("div");
+container.classList.add("container");
 document.querySelector("body").appendChild(container);
 
 let rowOrigin=document.createElement("div");
-rowOrigin.setAttribute("style", "display: flex; margin: 5px; border: solid green 5px; width:500px");
+rowOrigin.classList.add("row");
 
-let blockOrigin= document.createElement("div");
-blockOrigin.setAttribute("style", "height: 100px; width: 100px; margin: 10px; border: double pink 5px;");
-blockOrigin.classList.add("block");
+
+let block= document.createElement("div");
+//block.setAttribute("style", "height: 100px; width: 100px; margin: 10px; border: double pink 5px;");
+block.classList.add("block");
 
 //rowOrigin.appendChild(blockOrigin);
 //container.appendChild(rowOrigin);
-let test= document.querySelector(".test");
-
-test.addEventListener("mouseenter", ()=>{
-    test.setAttribute("style", "font-size: 40px");
-});
-
-test.addEventListener("mouseleave", ()=>{
-     test.setAttribute("style", "font-size: 20px");
-});
 
 
-/*
-buttons.forEach((button) => {
-  // and for each one we add a 'click' listener
-  button.addEventListener("click", () => {
-    alert(button.id);
-  });
-});
-*/
-
-for(let i=0; i< column; i++){
-    let blockClone= blockOrigin.cloneNode(true);
+for(let i=0; i< col; i++){
+    let blockClone= block.cloneNode(true);
     rowOrigin.appendChild(blockClone);
 }
+
 for(let i=0; i<row; i++){
     let rowClone= rowOrigin.cloneNode(true);
     container.appendChild(rowClone);
 }
 
-let blockTest= document.querySelectorAll(".block");
-blockTest.forEach((oneBlock)=>{
-    let value= oneBlock.addEventListener("mouseenter", ()=>{
-        oneBlock.textContent="The mouse touch me";
+//container.appendChild(rowOrigin);
+
+let blocks= document.querySelectorAll(".block");
+blocks.forEach((blockTemp)=>{
+    blockTemp.addEventListener("mouseenter", ()=>{
+        blockTemp.textContent="The mouse touch me";
     });
-    value=null;
 });
 
-blockTest.forEach((oneBlock) =>{
-    let value= oneBlock.addEventListener("moveleave", ()=>{
-        oneBlock.textContent="";
+blocks.forEach((blockTemp) =>{
+    blockTemp.addEventListener("mouseleave", ()=>{
+        blockTemp.textContent="";
     });
-    value=null;
 });
-
-/*
-let elements= container.querySelectorAll("block");
-elements.forEach((ele) =>{
-    ele.addEventListener("mouseover", ()=>{
-        ele.setAttribute("style", "border: solid black 5px");
-    });
-
-});
-*/
